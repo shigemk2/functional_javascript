33703250aff709281221bc2a6f227759afa4d25a
shigemk2 <i.am.shige@gmail.com>
2015-04-11 01:29:35 +0900

    splat

---
+++ chapter01/splat.js
@@ -0,0 +1,8 @@
+function splat(fun) {
+  return function(array) {
+    return fun.apply(null, array);
+  };
+}
+var addArrayElements = splat(function(x, y) { return x + y });
+console.log(addArrayElements([1, 2]));
+//=> 3
