4aeefd3904c12920b6ed2be673b78ec862160049
shigemk2 <i.am.shige@gmail.com>
2015-04-11 01:16:16 +0900

    times _.noConflict

---
+++ prefix/times_no_conflict.js
@@ -0,0 +1,8 @@
+var _ = require('underscore');
+
+var underscore = _.noConflict();
+underscore.times(4, function() { console.log("Major") });
+//（コンソール）Major
+//（コンソール）Major
+//（コンソール）Major
+//（コンソール）Major
