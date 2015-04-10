2c35661c38b20bb7ae7f4532eb00068926ef1025
shigemk2 <i.am.shige@gmail.com>
2015-04-11 00:55:58 +0900

    each

--- sample/array.js
+++
@@ -1,6 +0,0 @@
-var _ = require('underscore');
-// 配列
-var array = [5, 10, 15];
-_.each(array, function(element, index, array) {
-  console.log(element + ' : ' + index);
-});
--- sample/each.js
+++ sample/each.js
@@ -1,4 +1,10 @@
 var _ = require('underscore');
+// 配列
+var array = [5, 10, 15];
+_.each(array, function(element, index, array) {
+  console.log(element + ' : ' + index);
+});
+
 // オブジェクト
 var object = {
   first: 1,
